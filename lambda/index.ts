391938229186de4485701469d2f9ef672e1a7acb
kanadome <tsuchiyakeita2025@gmail.com>
2024-11-25 17:25:51 +0900

    lambdaのランダム情報を取得してみる

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,7 +1,13 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/aws-lambda'
+import { logger } from 'hono/logger'
+import * as os from 'os'
 
 const app = new Hono()
+
+interface UserBody {
+  user: string;
+}
 
 app.get('/', (c) => {
   return c.text('Hello Hono from AWS Lambda!')
@@ -9,6 +15,19 @@ app.get('/', (c) => {
 
 app.get('/hello', (c) => {
   return c.text('Hello Hono from AWS Lambda!!!!')
+})
+
+app.get('/runtime', (c) => {
+  const runtimeInfo = {
+    platform: os.platform(),
+    release: os.release(),
+    arch: os.arch(),
+    uptime: os.uptime(),
+    memory: os.totalmem(),
+    freeMemory: os.freemem(),
+    cpus: os.cpus().length
+  }
+  return c.json(runtimeInfo)
 })
 
 export const handler = handle(app)
