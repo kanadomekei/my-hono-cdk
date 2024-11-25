37fcd7318fe55bec5ca2bf4f169bab0bad2dfebe
kanadome <tsuchiyakeita2025@gmail.com>
2024-11-26 00:09:23 +0900

    cloudwatchを試してみる

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -26,15 +28,46 @@ export class CdkStack extends cdk.Stack {
       },
     });
 
+    // Lambdaのロググループを作成
+    const logGroup = new logs.LogGroup(this, 'LambdaLogGroup', {
+      logGroupName: `/aws/lambda/${fn.functionName}`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // 開発環境用
+    });
+
     // DynamoDB アクセス権限を Lambda に付与
     table.grantReadWriteData(fn);
 
-    fn.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE
+    // API Gateway用のロールを作成
+    const apigwLogRole = new iam.Role(this, 'ApiGatewayLoggingRole', {
+      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonAPIGatewayPushToCloudWatchLogs')
+      ]
     });
 
-    new apigw.LambdaRestApi(this, 'myapi', {
+    // アカウントレベルの設定を更新
+    const cfnAccount = new apigateway.CfnAccount(this, 'ApiGatewayAccount', {
+      cloudWatchRoleArn: apigwLogRole.roleArn
+    });
+
+    // API Gatewayの作成
+    const api = new apigateway.LambdaRestApi(this, 'myapi', {
       handler: fn,
+      deployOptions: {
+        stageName: 'prod',
+        loggingLevel: apigateway.MethodLoggingLevel.INFO,
+        dataTraceEnabled: true,
+        accessLogDestination: new apigateway.LogGroupLogDestination(new logs.LogGroup(this, 'ApiGatewayAccessLogs')),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields(),
+      },
+    });
+
+    // API Gateway作成時にアカウント設定への依存関係を追加
+    api.node.addDependency(cfnAccount);
+
+    // Lambda関数のURLを追加
+    fn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
     });
   }
 }
